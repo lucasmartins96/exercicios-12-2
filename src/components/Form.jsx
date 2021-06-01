b846ca0e20a40ce17275151006eb5465f1d5985a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-01 19:07:01 -0300

    substituindo os arquivos para os arquivos corretos

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,45 +1,30 @@
-import React, { Component } from 'react'
-import EstadoFavorito from './EstadoFavorito';
-import Idade from './Idade';
-import VaiComparecer from './VaiComparecer';
+import React, { Component } from 'react';
+import PersonalForm from './PersonalForm'
+import ProfessionalForm from './ProfessionalForm'
 
 class Form extends Component {
-  constructor() {
-    super();
-
-    this.handleChange = this.handleChange.bind(this);
-
-    this.state = {
-      estadoFavorito: '',
-      idade: 0,
-      vaiComparecer: false,
-    };
-  }
-
-
-  handleChange({ target }) {
-    const { name } = target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({
-      [name]: value,
-    });
-  }
-
   render() {
-    const { estadoFavorito, idade, vaiComparecer} = this.state;
+    const { sendForm, resetForm, changeHandler, currentState, onBlurHandler } = this.props;
+
     return (
-      <div>
-        <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
-        <form className="form">
-          <EstadoFavorito value={ estadoFavorito } handleChange={ this.handleChange }/>
-          <br/>
-          <Idade value={ idade } handleChange={ this.handleChange }/>
-          <br/>
-          <VaiComparecer value={ vaiComparecer } handleChange={ this.handleChange }/>
-          <br/>
-          <button>Inscrever</button>
-        </form>
-      </div>
+      <form>
+        <PersonalForm
+          changeHandler={ changeHandler }
+          onBlurHandler= { onBlurHandler }
+          currentState= { currentState }
+        />
+        <ProfessionalForm changeHandler={ changeHandler } />
+        <input
+            type="button"
+            onClick={ sendForm }
+            value="Enviar"
+        />
+        <input
+          type="reset"
+          onClick={ resetForm }
+          value="Limpar"
+        />
+      </form>
     );
   }
 }
